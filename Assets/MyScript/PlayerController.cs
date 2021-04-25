cc35fe0bb29bdb5ad8795c31c191d697881ac091
as <>
2021-04-25 20:47:45 +0900

    idleとwalkアニメーションの実装、プレイヤーを動作させる

---
+++ Assets/MyScript/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    float x;
+    float z;
+    public float moveSpeed = 2f;
+
+    Rigidbody rb;
+
+    Animator animator;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        x = Input.GetAxisRaw("Horizontal");
+        z = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 direction = transform.position + new Vector3(x, 0, z);
+        transform.LookAt(direction);
+        rb.velocity = new Vector3(x, 0, z) * moveSpeed;
+        animator.SetFloat("Speed", rb.velocity.magnitude);
+    }
+}

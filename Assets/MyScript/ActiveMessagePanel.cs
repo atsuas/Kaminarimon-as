25f7b7ff146846b7d5e0434b8299d1c913743961
as <>
2021-08-05 01:07:00 +0900

    開始時にテキストを表示する

---
+++ Assets/MyScript/ActiveMessagePanel.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActiveMessagePanel : MonoBehaviour
+{
+
+	//　MessageUIに設定されているMessageスクリプトを設定
+	[SerializeField]
+	private Message messageScript;
+
+	//　表示させるメッセージ
+	private string message = "かかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかかか"
+							 + "ききききききききききききききききききききききききききききききききききききききききききききききききききききききき\n"
+							 + "くくくくく\n"
+							 + "けけけけけけけけけけけけ";
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			messageScript.SetMessagePanel(message);
+		}
+	}
+}
+
